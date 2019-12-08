a48bd995c10279cd93cf7d2cf9331276e5f1557f
Spencer Stolworthy <sdstolworthy@gmail.com>
2019-12-07 17:10:58 -0700

    fixes for image height

--- src/components/zulu-ui/inputs/Sketchpad.stories.tsx
+++ src/components/zulu-ui/inputs/Sketchpad.stories.tsx
@@ -1,16 +1,28 @@
 import * as React from 'react';
-import { withKnobs, color, number } from '@storybook/addon-knobs';
-import { Sketchpad } from './Sketchpad';
+import {withKnobs, color, number, text} from '@storybook/addon-knobs';
+import {Sketchpad} from './Sketchpad';
 
-export default { title: 'Zentake Design Language|Atoms/Sketchpad', decorators: [withKnobs] };
-
+export default {
+  title: 'Zentake Design Language|Atoms/Sketchpad',
+  decorators: [withKnobs],
+};
+const DEFAULT_IMAGE_URL =
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Human_skeleton_front_no-text_no-color.svg/1200px-Human_skeleton_front_no-text_no-color.svg.png';
 export const sketchPad = () => (
   <div
-    style={{ position: 'relative' }}>
-    <Sketchpad style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }} imageUrl="https://via.placeholder.com/1000"
-      strokeColor={color("Stroke Color", "#000000")}
-      strokeWidth={number("Stroke Width", 3, { min: 0, max: 10, step: 1.0 })}
+    style={{
+      position: 'relative',
+      maxWidth: 'fit-content',
+      height: text('Image Height', '600px'),
+    }}>
+    <Sketchpad
+      style={{position: 'absolute', top: 0, left: 0, right: 0, bottom: 0}}
+      strokeColor={color('Stroke Color', '#000000')}
+      strokeWidth={number('Stroke Width', 3, {min: 0, max: 10, step: 1.0})}
     />
-    <img src="https://via.placeholder.com/400" />
+    <img
+      style={{maxWidth: '100%', maxHeight: '100%', objectFit: 'contain'}}
+      src={text('Image', DEFAULT_IMAGE_URL)}
+    />
   </div>
 );
--- src/components/zulu-ui/inputs/Sketchpad.tsx
+++ src/components/zulu-ui/inputs/Sketchpad.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
 
-export type Point = { x: number; y: number };
+export type Point = {x: number; y: number};
 
 export interface SketchpadProps {
-  imageUrl: string;
   strokeColor: string;
   strokeWidth: number;
-  style?: React.CSSProperties
+  style?: React.CSSProperties;
 }
 interface SketchpadState {
   lines: Point[][];
@@ -22,15 +21,19 @@ class Sketchpad extends React.Component<SketchpadProps, SketchpadState> {
     };
   }
 
-
-
-  handleMouseDown = (mouseEvent: React.MouseEvent<SVGSVGElement> | React.TouchEvent<SVGSVGElement>) => {
+  handleMouseDown = (
+    mouseEvent:
+      | React.MouseEvent<SVGSVGElement>
+      | React.TouchEvent<SVGSVGElement>,
+  ) => {
+    mouseEvent.preventDefault()
     if ('button' in mouseEvent && mouseEvent.button != 0) {
+
       return;
     }
     const point = this.relativeCoordinatesForEvent(mouseEvent);
     if (!point) {
-      return
+      return;
     }
     this.setState(prevState => {
       return {
@@ -40,23 +43,28 @@ class Sketchpad extends React.Component<SketchpadProps, SketchpadState> {
     });
   };
   handleMouseUp = () => {
-    this.setState({ isDrawing: false });
+    this.setState({isDrawing: false});
   };
-  handleMouseMove = (mouseEvent: React.MouseEvent<SVGSVGElement> | React.TouchEvent<SVGSVGElement>) => {
+  handleMouseMove = (
+    mouseEvent:
+      | React.MouseEvent<SVGSVGElement>
+      | React.TouchEvent<SVGSVGElement>,
+  ) => {
+    mouseEvent.preventDefault()
     if (!this.state.isDrawing) {
       return;
     }
     const point = this.relativeCoordinatesForEvent(mouseEvent);
     if (!point) {
-      return
+      return;
     }
     this.setState(prevState => {
-      const currentLines = [...prevState.lines]
-      const lastLine = currentLines.splice(currentLines.length - 1, 1)
+      const currentLines = [...prevState.lines];
+      const lastLine = currentLines.splice(currentLines.length - 1, 1);
       if (!lastLine || !lastLine[0]) {
-        return prevState
+        return prevState;
       }
-      lastLine[0].push(point)
+      lastLine[0].push(point);
       return {
         ...prevState,
         lines: [...currentLines, ...lastLine],
@@ -64,32 +72,37 @@ class Sketchpad extends React.Component<SketchpadProps, SketchpadState> {
     });
   };
 
-  getXYForTouchOrClick = (event: React.TouchEvent<SVGSVGElement> | React.MouseEvent<SVGSVGElement>): Point | null => {
+  getXYForTouchOrClick = (
+    event: React.TouchEvent<SVGSVGElement> | React.MouseEvent<SVGSVGElement>,
+  ): Point | null => {
     if ('touches' in event) {
-      const touchEvent = event as React.TouchEvent<SVGSVGElement>
+      const touchEvent = event as React.TouchEvent<SVGSVGElement>;
       if (touchEvent.touches.length > 0) {
-        return { x: touchEvent.touches[0].clientX, y: touchEvent.touches[0].clientY }
+        return {
+          x: touchEvent.touches[0].clientX,
+          y: touchEvent.touches[0].clientY,
+        };
       }
     }
     if ('clientX' in event && 'clientY' in event) {
-      const mouseEvent = event as React.MouseEvent<SVGSVGElement>
-      return { x: mouseEvent.clientX, y: mouseEvent.clientY }
+      const mouseEvent = event as React.MouseEvent<SVGSVGElement>;
+      return {x: mouseEvent.clientX, y: mouseEvent.clientY};
     }
-    return null
-  }
+    return null;
+  };
 
   relativeCoordinatesForEvent = (
-    mouseEvent: React.MouseEvent<SVGSVGElement> | React.TouchEvent<SVGSVGElement>,
+    mouseEvent:
+      | React.MouseEvent<SVGSVGElement>
+      | React.TouchEvent<SVGSVGElement>,
   ) => {
     if (!this.drawingArea || !this.drawingArea.current) {
       return;
     }
     const boundingRect = this.drawingArea.current.getBoundingClientRect();
-    const eventXY = this.getXYForTouchOrClick(mouseEvent)
-    if (
-      !eventXY
-    ) {
-      return null
+    const eventXY = this.getXYForTouchOrClick(mouseEvent);
+    if (!eventXY) {
+      return null;
     }
     return Object.freeze({
       x: eventXY.x - boundingRect.left,
@@ -97,38 +110,67 @@ class Sketchpad extends React.Component<SketchpadProps, SketchpadState> {
     });
   };
   render() {
-    const { strokeColor = "black", strokeWidth = 3 } = this.props;
+    const {strokeColor = 'black', strokeWidth = 3} = this.props;
     return (
       <svg
-        style={{ height: '100%', width: '100%', ...(this.props.style || {}) }}
+        style={{touchAction: 'none', ...(this.props.style || {})}}
         ref={this.drawingArea}
         onMouseDown={this.handleMouseDown}
         onMouseMove={this.handleMouseMove}
         onMouseUp={this.handleMouseUp}
         onTouchStart={this.handleMouseDown}
         onTouchEnd={this.handleMouseUp}
-        onTouchMove={this.handleMouseMove}
-      >
+        onTouchMove={this.handleMouseMove}>
         {this.state.lines.map((line, index) => (
-          <DrawingLine color={strokeColor} line={line} strokeWidth={strokeWidth} key={index} />
+          <DrawingLine
+            color={strokeColor}
+            line={line}
+            strokeWidth={strokeWidth}
+            key={index}
+          />
         ))}
-      </ svg>
+      </svg>
     );
   }
 }
 
-const Drawing = ({ lines, strokeColor, strokeWidth, children }: { lines: Point[][], strokeColor: string, strokeWidth: number, children: React.ReactNode }) => {
+const Drawing = ({
+  lines,
+  strokeColor,
+  strokeWidth,
+  children,
+}: {
+  lines: Point[][];
+  strokeColor: string;
+  strokeWidth: number;
+  children: React.ReactNode;
+}) => {
   return (
     <svg>
       {lines.map((line, index) => (
-        <DrawingLine color={strokeColor} line={line} strokeWidth={strokeWidth} key={index} />
+        <DrawingLine
+          color={strokeColor}
+          line={line}
+          strokeWidth={strokeWidth}
+          key={index}
+        />
       ))}
     </svg>
   );
 };
 
-const DrawingLine = ({ line, color, strokeWidth }: { line: Point[], color: string, strokeWidth: number }) => {
+const DrawingLine = ({
+  line,
+  color,
+  strokeWidth,
+}: {
+  line: Point[];
+  color: string;
+  strokeWidth: number;
+}) => {
   const pathData = `M ${line.map(p => `${p.x} ${p.y}`).join(' L ')}`;
-  return <path fill="none" stroke={color} strokeWidth={strokeWidth} d={pathData} />;
+  return (
+    <path fill="none" stroke={color} strokeWidth={strokeWidth} d={pathData} />
+  );
 };
-export { Sketchpad };
+export {Sketchpad};
